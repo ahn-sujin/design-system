0763a52777d0b6258815984c2dcf3b18729ff8df
ahn-sujin <asj9674@gmail.com>
2024-12-04 22:11:34 +0900

    Fix:mdx -> stories autoDocs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,12 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../src/stories/**/*.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/react-vite",
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import reactMdx from "vite-plugin-react-mdx";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), reactMdx()],
+  plugins: [react()],
 });
